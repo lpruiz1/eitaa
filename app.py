f42a3a157b866f6ed725f84afc3c8a04aeb10c21
Luis Gama <2023004864@ifam.edu.br>
2023-08-31 01:46:02 +0000

    primeira aula

---
+++ app.py
@@ -0,0 +1,5 @@
+from flask import Flask
+app=Flask("meu app")
+@app.route('/')
+def hello():
+    return "OIII EU AMO A LARISSAAAA A GENTE VAI CASAAAAAAAAAAAAAAAAAAARAR  AODFJALSKD NFAKJFIjddnFNJdvnad>FNAJWdf"
